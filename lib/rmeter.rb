bbf656b95b4367d325f0a6afe3981bc8a9a5d70d
Paul Guelpa <paul.guelpa@gmail.com>
2011-07-02 18:12:27 -0400

    Finishing up initial parser details

--- lib/rmeter.rb
+++ lib/rmeter.rb
@@ -1,4 +1,4 @@
 require 'active_support/core_ext/enumerable'
 
 require 'rmeter/version'
-require 'rmeter/run/parser'
+require 'rmeter/parser'
--- spec/rmeter/parser_spec.rb
+++ spec/rmeter/parser_spec.rb
@@ -1,19 +1,20 @@
 require 'spec_helper'
 
-describe Rmeter::Run::Parser do
+describe Rmeter::Parser do
   describe "#parse" do
     it "should return an array of hashes with the parsed values" do
-      xml =<<XML
+      xml =<<-XML
       <?xml version="1.0" encoding="UTF-8"?>
       <testResults version="1.2">
-      <httpSample t="780" lt="746" ts="1309493042836" s="true" lb="Home Page" rc="200" rm="OK" tn="First Group 1-1" dt="text" by="9861"/>
+        <httpSample t="780" lt="746" ts="1309493042836" s="true" lb="Home Page" rc="200" rm="OK" tn="First Group 1-1" dt="text" b="9861"/>
       </testResults>
-XML
+      XML
+
       File.should_receive(:open).with('/some/file/path.xml').and_return(StringIO.new(xml))
-      Rmeter::Run::Parser.new('/some/file/path.xml').parse.should == [
+      Rmeter::Parser.new('/some/file/path.xml').parse.should == [
         {
           :time => 780,
-          :latency => 780,
+          :latency => 746,
           :timestamp => Time.at(1309493042836 / 1000),
           :success => true,
           :label => 'Home Page',
